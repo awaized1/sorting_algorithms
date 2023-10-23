82a12886cb6a2f4a71b132840eb95acb6b283c8b
awaized1 <aduolaayomide@gmail.com>
2023-10-23 02:23:31 +0000

    new

---
+++ sort.h
@@ -0,0 +1,35 @@
+#ifndef SORT_H
+#define SORT_H
+#include <stdlib.h>
+
+/**
+ * struct listint_s - A doubly linked list
+ * @n: int saved in node
+ * @prev: Pointer to the previous node of the list
+ * @next: Pointer to the next node of the list
+ */
+
+typedef struct listint_s
+{
+	const int n;
+	struct listint_s *prev;
+	struct listint_s *next;
+} listint_t;
+
+void bubble_sort(int *array, size_t size);
+void print_array(const int *array, size_t size);
+void print_list(const listint_t *list);
+listint_t *swap_node(listint_t *node, listint_t **list);
+void insertion_sort_list(listint_t **list);
+void selection_sort(int *array, size_t size);
+void quick_sort(int *array, size_t size);
+void shell_sort(int *array, size_t size);
+void cocktail_sort_list(listint_t **list);
+void counting_sort(int *array, size_t size);
+void merge_sort(int *array, size_t size);
+void heap_sort(int *array, size_t size);
+void radix_sort(int *array, size_t size);
+void bitonic_sort(int *array, size_t size);
+void quick_sort_hoare(int *array, size_t size);
+
+#endif
